51bb2448c93f338e9c3961632edce160415c3a48
sethcarl2000 <sethhall@jlab.org>
2025-09-13 17:01:14 -0400

    Add first version of 'print_legendre_roots.py', to print out csv file of gaussian quadrature weights

---
+++ print_legendre_roots.py
@@ -0,0 +1,41 @@
+import numpy as np
+# import sys,argparse
+from scipy.special import roots_legendre, eval_legendre
+import csv 
+
+if __name__ == "__main__": 
+    
+    # get the max number of points to print
+    max_order = 10
+    # if len(sys.argv) > 1: 
+    # max_order = sys.argv[1]
+     
+    name = "legenre_roots_" + str(max_order) + ".dat"
+
+    # now, write the outputs for each line
+    with open(name, 'w', newline='') as f:
+
+        writer = csv.writer(f, delimiter=' ')
+
+        # get the roots for each order
+        for order in range(1, max_order+1): 
+            
+            # get the roots of this order
+            roots, weights = roots_legendre(order)
+
+            writer.writerow(["order", order])
+
+            # now, print the result
+            for row in range(0, len(roots)): 
+                
+                writer.writerow([roots[row], weights[row]])
+            
+            writer.writerow([])
+
+
+
+
+
+    
+
+
