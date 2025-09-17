6c8c96231b7ad32480d84232a97afc123ba80540
sethcarl2000 <sethhall@jlab.org>
2025-09-17 12:29:44 -0400

    Add exponential-spacing of N values, to allow efficient computation of Nvalues in the 1e6 range for trapezoids

--- test_integration.cpp
+++ test_integration.cpp
@@ -25,8 +25,38 @@ double fcn_indef_integral(double x) { return -1.*exp(-1.*x); }
 // 1. simpson's rule needs at least 3.
 // 2. simpson's rule can only work with an odd number of points, so both 'N_min' and 'N_max' must be odd. 
 // 3. if 'N_max' is more than the biggest order of gauss quad-weights given in 'gauss_quad_points_[N].dat', then an error will be thrown. 
-const int N_min = 3;
-const int N_max = 159; 
+
+vector<int> Get_integration_steps(int n_min, const int npts, const bool odd_only=false) 
+{
+    vector<int> Nvals; 
+
+    if (odd_only && n_min % 2 == 0) n_min +=1; 
+
+    for (int i=0; i<npts; i++) {
+        Nvals.push_back(odd_only ? n_min + 2*i : n_min + i); 
+    }
+    return Nvals; 
+}
+
+vector<int> Get_integration_steps_exponential(int n_min, const int npts, const double step=1.05, const bool odd_only=false)
+{
+    if (odd_only && n_min % 2 == 0) n_min +=1; 
+
+    vector<int> Nvals{ n_min }; 
+
+    double N_double = (double)Nvals.back(); 
+
+    for (int i=0; i<npts; i++) {
+    
+        N_double *= step; 
+        int new_val = (int)round(N_double);
+        //for small step sizes, this may not produce a new integers if the starting ints are small.
+        //this bit of logic is to avoid having the same integer be double-counted
+        if ( new_val > Nvals.back() ) Nvals.push_back( (odd_only && new_val % 2==0) ? new_val+1 : new_val ); 
+    }
+
+    return Nvals; 
+}
 
 //integration limits: 
 const double x_min = 0.; 
@@ -41,23 +71,29 @@ const char* path_outgraphic = "Errors.png";
 int main(int argc, char* argv[])
 {
     //create a vector of evenly-spaced, odd N-values
-    vector<int> N_vals; 
-    for (int N=N_min; N<=N_max; N += 2) { N_vals.push_back(N); } 
+    vector<int> Nvals_trap  = Get_integration_steps_exponential(2, 400, 1.035, false);  
+    vector<int> Nvals_simp  = Get_integration_steps_exponential(3, 250, 1.035, true); 
+    vector<int> Nvals_gauss = Get_integration_steps(2, 160); 
 
     //initialize the gaussian integrator
     GaussInt gauss_int(path_quadpoints); 
 
+    const double integral_analytical = fcn_indef_integral(x_max) - fcn_indef_integral(x_min); 
+
     //h-vals and erorrs are drawn in a log-log plot. 
     vector<double> error_trap{}, error_simp{}, error_gauss{};  
+    
+    for (int N : Nvals_trap) error_trap.push_back(
+        fabs((trapez (fcn, N, x_min,x_max) - integral_analytical) / integral_analytical)
+    );
 
-    for (int N : N_vals) {
+    for (int N : Nvals_simp) error_simp.push_back(
+        fabs((simpson(fcn, N, x_min,x_max) - integral_analytical) / integral_analytical)
+    );
 
-        double analytical = fcn_indef_integral(x_max) - fcn_indef_integral(x_min); 
-
-        error_trap .push_back( fabs((trapez (fcn, N, x_min,x_max) - analytical) / analytical) ); 
-        error_simp .push_back( fabs((simpson(fcn, N, x_min,x_max) - analytical) / analytical) ); 
-        error_gauss.push_back( fabs((gauss_int.Integral(fcn, N, x_min,x_max) - analytical) / analytical) ); 
-    }   
+    for (int N : Nvals_gauss) error_gauss.push_back(
+        fabs((gauss_int.Integral(fcn, N, x_min,x_max) - integral_analytical) / integral_analytical)
+    );
 
     //find the minimum and maximum 
     double y_min{1e30}, y_max{-1e30}; 
@@ -71,26 +107,25 @@ int main(int argc, char* argv[])
     for (auto y : error_simp)  find_min_max(y); 
     for (auto y : error_gauss) find_min_max(y); 
 
-    vector<double> N_vals_double; 
-    for (int N : N_vals) N_vals_double.push_back((double)N); 
+    auto Make_vec_double = [](const vector<int>& V) { 
+        vector<double> Vd; 
+        for (int i : V) Vd.push_back((double)i); 
+        return Vd; 
+    }; 
 
-    for (int i=0; i<error_trap.size(); i++) {
-        printf("n, trap,simp,gauss:  %3i : %-.4e, %-.4e, %-.4e\n", 
-            N_vals[i], 
-            error_trap[i],
-            error_simp[i],
-            error_gauss[i]
-        ); 
-    }
-    
+    vector<double>&& Nvals_double_trap  = Make_vec_double(Nvals_trap); 
+    vector<double>&& Nvals_double_simp  = Make_vec_double(Nvals_simp); 
+    vector<double>&& Nvals_double_gauss = Make_vec_double(Nvals_gauss); 
+
     auto canv = new TCanvas; 
 
     //now, create the graphs
-    auto graph_trap   = new TGraph( N_vals.size(), N_vals_double.data(), error_trap.data() );
-    auto graph_simp   = new TGraph( N_vals.size(), N_vals_double.data(), error_simp.data() );
-    auto graph_extrap = new TGraph( N_vals.size(), N_vals_double.data(), error_gauss.data() );
+    auto graph_trap   = new TGraph( Nvals_trap.size(),  Nvals_double_trap.data(),  error_trap.data() );
+    auto graph_simp   = new TGraph( Nvals_simp.size(),  Nvals_double_simp.data(),  error_simp.data() );
+    auto graph_extrap = new TGraph( Nvals_gauss.size(), Nvals_double_gauss.data(), error_gauss.data() );
 
     //set the log scale for x & y 
+    gPad->SetLogx(1); 
     gPad->SetLogy(1); 
 
     //set margin so we can see the histo titles
